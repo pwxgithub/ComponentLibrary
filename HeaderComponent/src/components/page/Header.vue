ef9df84208edc02da5745f3a48ffd7b9fdca683e
pwx <pwx_MailBox@163.com>
2020-12-25 17:43:48 +0800

    🦄refactor(修改路由、添加DashBoard和Header组件):

---
+++ HeaderComponent/src/components/page/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: '1111'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ HeaderComponent/src/view/dashboard/DashBoard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="dashboard">
+    {{msg}}
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/page/Header'
+
+export default {
+  name: 'DashBoard',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
